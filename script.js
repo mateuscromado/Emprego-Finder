31c660ed9767c02737fa4788beaedd917cfc58ac
Mateus Pereira <118226023+mateuscromado@users.noreply.github.com>
2023-11-22 19:14:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -40,15 +40,8 @@ function areaEnviar(){
    if(areaAtuacao == "" || idade == "" || regiao == ""){
     alert("Preencha os campos")
    }else{
-   // window.location.href = "index.html"
-    console.log(nome)
-    console.log(sobrenome)
-    console.log(email)
-    console.log(senha)
-    console.log(areaAtuacao)
-    console.log(idade)
-    console.log(regiao)
-
+   window.location.href = "index.html"
+  
 
    }
 
